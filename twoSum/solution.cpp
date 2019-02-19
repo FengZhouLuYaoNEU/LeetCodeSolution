7e99f0d82ad246761a72820a91dd0581313f6df2
ChaochaoLi <lichao694@126.com>
2019-02-19 19:39:49 +0800

    [task] add a new method for twoSum

--- twoSum/solution.cpp
+++ twoSum/solution.cpp
@@ -18,3 +18,21 @@ public:
       return ret;
     }
 };
+// a new method 
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+      
+      size_t size = nums.size();
+      unordered_map<int,int>  tmp_map;
+      for (size_t i = 0; i < size; i++)
+      {
+          if (tmp_map.count(target - nums[i]))
+          {
+              return vector<int> {tmp_map[target - nums[i]],i};
+          }
+          tmp_map[nums[i]] = i;
+      }
+      return vector<int>();
+    }
+};
